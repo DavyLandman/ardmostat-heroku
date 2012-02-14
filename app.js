599656cbaa6d3cf370cee395edd09c5c6aba85fa
Davy Landman <davy.landman@gmail.com>
2012-02-14 11:26:14 +0100

    Now using the env.PORT

--- app.js
+++ app.js
@@ -39,5 +39,7 @@ mainRoutes.init(app);
 tempRoutes.init(app);
 
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+	console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
