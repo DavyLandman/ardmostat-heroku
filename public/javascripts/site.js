901d5f21451eaf61f6597e540a0245820a8e6a91
Davy Landman <davy.landman@gmail.com>
2012-04-15 12:02:41 +0200

    Added sparkline temperature log to each page

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,6 +1,10 @@
 $(function() {
 	var now = new Date();
-	$.get('/CurrentTemperature?time=' + new Date(now.setSeconds(Math.round(now.getSeconds() / 4) * 4)).toISOString(), function (r) {
-		$('#currentTemperature').text(r);
+	$.get('/RecentTemperatures?time=' + new Date(now.setSeconds(Math.round(now.getSeconds() / 4) * 4)).toISOString(), function (r) {
+		var temperatures = [];
+		for (var i =0; i < r.length; i++ ) {
+			temperatures.push(r[i][1]);
+		}
+		$('#currentTemperature').sparkline(temperatures, { fill: false});
 	});
 });
