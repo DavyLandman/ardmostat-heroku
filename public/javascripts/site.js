319027aa69bb22fd0917f22e541e94c4f9207e7a
Davy Landman <davy.landman@gmail.com>
2012-04-16 09:28:36 +0200

    Added updating sparkline temperature to header

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,10 +1,15 @@
 $(function() {
-	var now = new Date();
-	$.get('/RecentTemperatures?time=' + new Date(now.setSeconds(Math.round(now.getSeconds() / 4) * 4)).toISOString(), function (r) {
-		var temperatures = [];
-		for (var i =0; i < r.length; i++ ) {
-			temperatures.push(r[i][1]);
-		}
-		$('#currentTemperature').sparkline(temperatures, { fill: false});
-	});
+	function getRecentTemperatures() {	
+		var now = new Date();
+		$.get('/RecentTemperatures?time=' + new Date(now.setSeconds(Math.round(now.getSeconds() / 4) * 4)).toISOString(), function (r) {
+			var temperatures = [];
+			for (var i =0; i < r.length; i++ ) {
+				temperatures.push(r[i][1]);
+			}
+			$('#recentTemperatures').sparkline(temperatures, { fill: false});
+			$('#currentTemperature').text(temperatures.pop() + ' c');
+		});
+	}
+	setInterval(getRecentTemperatures, 10000);
+	getRecentTemperatures();
 });
