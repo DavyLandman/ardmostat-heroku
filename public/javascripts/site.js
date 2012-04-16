1bed4ad319726a79d2631be6a601a1f85fee91f5
Davy Landman <davy.landman@gmail.com>
2012-04-16 13:51:39 +0200

    Improved layout of the sparklines

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -3,11 +3,21 @@ $(function() {
 		var now = new Date();
 		$.get('/RecentTemperatures?time=' + new Date(now.setSeconds(Math.round(now.getSeconds() / 4) * 4)).toISOString(), function (r) {
 			var temperatures = [];
+			var min = r[0][1];
+			var max = r[0][1];
 			for (var i =0; i < r.length; i++ ) {
-				temperatures.push(r[i][1]);
+				var currentTemp = r[i][1];
+				if (currentTemp > max) {
+					max = currentTemp;
+				}
+				else if (currentTemp < min) {
+					min = currentTemp;
+				}
+				temperatures.push(currentTemp);
 			}
 			$('#recentTemperatures').sparkline(temperatures, { fill: false});
-			$('#currentTemperature').text(temperatures.pop() + ' c');
+			$('#minTemperature').text(min);
+			$('#maxTemperature').text(max);
 		});
 	}
 	setInterval(getRecentTemperatures, 10000);
